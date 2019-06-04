b0dbd9ae0247bb714f463fde04eaa427d0b14780
Szewa <marcin91.szewczyk@gmail.com>
2019-06-04 20:32:40 +0200

    Save user to local storage

---
+++ frontend/src/app/auth/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '~/app/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const url = state.url;
+    return this.checkLogin(url);
+  }
+
+  private checkLogin(url: string): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    }
+    this.authService.redirectedFrom = url;
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
+
