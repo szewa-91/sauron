4a598d9003ac728b9b2fb59ca62ac31877667765
Marcin <mlopatka.krk@gmail.com>
2019-06-30 17:03:12 +0200

    Code review - CHANGE

--- server/src/main/java/com/sauron/fake/util/FakeBanksUtils.java
+++ server/src/main/java/com/sauron/fake/util/FakeBanksUtils.java
@@ -1,17 +1,19 @@
 package com.sauron.fake.util;
 
-import static com.sauron.fake.util.FakeBanksConstants.ACCOUNT_NUMBERS;
-import static com.sauron.fake.util.FakeBanksConstants.ALLOWED_DIRECTIONS;
-import static com.sauron.fake.util.FakeBanksConstants.CURRENT_USERS;
-import static com.sauron.fake.util.FakeBanksConstants.PAYMENT_TITLES;
+import com.sauron.fake.model.ExternalTransaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
-import com.sauron.fake.model.ExternalTransaction;
+import static com.sauron.fake.util.FakeBanksConstants.ACCOUNT_NUMBERS;
+import static com.sauron.fake.util.FakeBanksConstants.ALLOWED_DIRECTIONS;
+import static com.sauron.fake.util.FakeBanksConstants.CURRENT_USERS;
+import static com.sauron.fake.util.FakeBanksConstants.PAYMENT_TITLES;
 
 /**
  * Created by marcin.lopatka on 27-06-2019
@@ -19,14 +21,15 @@ import com.sauron.fake.model.ExternalTransaction;
 public class FakeBanksUtils {
 	
 	public static List<ExternalTransaction> generateTransactions(int transactionCount, int maxTransactionAmount) {
-		List<ExternalTransaction> transactions = new ArrayList<>();
-		for(long i = 0; i < transactionCount; i++ ) {
-			transactions.add(new ExternalTransaction(i, generateElement(CURRENT_USERS), generateElement(PAYMENT_TITLES),
-					generateElement(ACCOUNT_NUMBERS), generateElement(ALLOWED_DIRECTIONS), generateAmount(maxTransactionAmount),
-					getTransactionDate(i)));
-		}
-		
-		return transactions;
+		return LongStream.range(0, transactionCount)
+				.mapToObj(i -> createTransaction(i, maxTransactionAmount))
+				.collect(Collectors.toList());
+	}
+
+	private static ExternalTransaction createTransaction(Long id, int maxTransactionAmount) {
+		return new ExternalTransaction(id, generateElement(CURRENT_USERS), generateElement(PAYMENT_TITLES),
+				generateElement(ACCOUNT_NUMBERS), generateElement(ALLOWED_DIRECTIONS), generateAmount(maxTransactionAmount),
+				getTransactionDate(id));
 	}
 	
 	public static BigDecimal generateAmount(int maxTransactionAmount) {
