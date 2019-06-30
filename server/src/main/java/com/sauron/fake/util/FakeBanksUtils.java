6eeaef1b66a5e1db1be91434e5f0b098e3290a01
Marcin <mlopatka.krk@gmail.com>
2019-06-30 17:06:51 +0200

    Code review - CHANGE

--- server/src/main/java/com/sauron/fake/util/FakeBanksUtils.java
+++ server/src/main/java/com/sauron/fake/util/FakeBanksUtils.java
@@ -4,7 +4,6 @@ import com.sauron.fake.model.ExternalTransaction;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
