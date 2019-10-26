4902100ca8740763782242bc2e8a0d1198114899
Szewa <marcin91.szewczyk@gmail.com>
2019-10-26 11:56:08 +0200

    Add material buttons

--- frontend/src/app/pages/login/login.module.ts
+++ frontend/src/app/pages/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material';
+import { MatButtonModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -10,6 +10,7 @@ import { MatInputModule } from '@angular/material';
     CommonModule,
     FormsModule,
     MatInputModule,
+    MatButtonModule,
   ],
   exports: [
     LoginComponent,
